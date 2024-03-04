4427ac6f8b7e4ea4423e9bf09bab9544fd312a0d
Wojciech Przybyła <contact@wpww.pl>
2024-03-04 23:09:43 +0100

    🚑 catching errors while generating data

--- includes/calculations.r
+++ includes/calculations.r
@@ -17,12 +17,6 @@ map(
 #### markov ####
 arma <- custom_read_rds("arma_aic")
 
-#### ! HEAVY FUNCTION ! ####
-# gameplan
-# 1. wyciągnąć reszty z modelu średniej
-# 2. wstawić je do garcha
-# 3. ...
-# 4. profit?
 for (table_name in tables) {
   pb <- progress_bar$new(
     total = nrow(markov_models_for_testing_indices),
@@ -36,22 +30,25 @@ for (table_name in tables) {
 
   pmap(
     markov_models_for_testing_indices,
-    function(Var1, Var2){
-      pb$tick()
+    possibly(
+      function(Var1, Var2) {
+        pb$tick()
 
-      CreateSpec(
-        variance.spec = list(model = c(
-          markov_models_for_testing[Var1, ]$volatility,
-          markov_models_for_testing[Var2, ]$volatility
-        )),
-        distribution.spec = list(distribution = c(
-          markov_models_for_testing[Var1, ]$distribution,
-          markov_models_for_testing[Var2, ]$distribution
-        )),
-        switch.spec = list(do.mix = FALSE, K = NULL)
-      ) %>%
-        FitML(series)
-    }
+        CreateSpec(
+          variance.spec = list(model = c(
+            markov_models_for_testing[Var1, ]$volatility,
+            markov_models_for_testing[Var2, ]$volatility
+          )),
+          distribution.spec = list(distribution = c(
+            markov_models_for_testing[Var1, ]$distribution,
+            markov_models_for_testing[Var2, ]$distribution
+          )),
+          switch.spec = list(do.mix = FALSE, K = NULL)
+        ) %>%
+          FitML(series)
+      },
+      otherwise = NULL
+    )
   ) %>%
     custom_save_rds(
       custom_paste_tight(
