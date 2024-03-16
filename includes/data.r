17192f8e176e9370e041a900e24f3e88fc8ca477
Wojciech Przybyła <contact@wpww.pl>
2024-03-16 21:40:38 +0100

    📝 added new data sources

--- includes/data.r
+++ includes/data.r
@@ -1,32 +1,57 @@
 #### load ####
-tables <- c("wig20", "pkn", "pko", "ebs", "dvl", "snt")
+tables <- c("wig", "pkn", "pko", "ebs", "dom", "sve")
 tables_full_names <- setNames(
   c(
     "WIG20",
     "PKN Orlen",
     "PKO BP",
     "11 bit studios",
-    "Develia",
-    "Synektik"
+    "Dom Development",
+    "Synthaverse"
   ),
   tables
 )
-for (i in seq_along(tables)) {
-  assign(
-    tables[i],
-    read.csv(paste("data/", tables[i], ".csv", sep = ""), sep = ";") %>%
-      mutate(return = log(Zamkniecie / lag(Zamkniecie)) * 100) %>%
-      mutate(across(matches(c("Data")), as_date)) %>%
-      mutate(index = tables[i])
-  )
-}
 
 #### gather ####
-data <- bind_rows(wig20, pkn, pko, ebs, dvl, snt) %>% as_tibble()
-returns_split <-
+data <-
+  map(tables, function(table_name) {
+    read.csv(c_paste_tight("data/", table_name, ".csv"), sep = ";") %>%
+      as_tibble() %>%
+      mutate(across(matches(c("Data")), as_date)) %>%
+      mutate(index = table_name)
+  }) %>%
+  bind_rows()
+
+garchx_periods <- list(
+  c("1.03.2017", "1.05.2017", "powstanie Krajowej Administracji Skarbowej w Polsce"),
+  c("4.03.2020", "1.06.2020", "wybuch pandemii koronawirusa w Polsce"),
+  c("1.10.2020", "15.02.2021", "???"),
+  c("1.4.2021", "31.05.2021", "???"),
+  c("24.02.2022", "1.05.2022", "początek inwazji rosyjskiej na Ukrainie")
+)
+
+data_split <-
   data %>%
     group_by(index) %>%
     nest() %>%
-    mutate(data = map(data, ~.x %>% pull(return))) %>%
+    mutate(data = map(data, function(d) {
+      # if (index %in% c("pkn", "dvl", "snt"))
+      #   d <- d %>% tail(1000)
+      d %>%
+        filter(Data %within% interval(
+          dmy(data_range[1]), dmy(data_range[2])
+        )) %>%
+        mutate(
+          return = log(Zamkniecie / lag(Zamkniecie)) * 100,
+          is_crisis = Reduce(`|`, map(
+            garchx_periods,
+            ~ Data %within% interval(dmy(.x[1]), dmy(.x[2]))
+          ))
+        )
+    })) %>%
     deframe()
-# names(returns_split) <- tables
+
+returns_split <- data_split %>%
+  map(~ pull(.x, return))
+garchx_externals <- data_split %>%
+  map(~ pull(.x, is_crisis) %>% as.numeric)
